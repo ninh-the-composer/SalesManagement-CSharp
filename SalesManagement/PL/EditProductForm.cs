5ee569d51294575544bcc032ae5bc778ebdc09d3
ninh137 <adzekk01@gmail.com>
2021-03-01 09:25:10 +0700

    Add Validation

--- SalesManagement/PL/EditProductForm.cs
+++ SalesManagement/PL/EditProductForm.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 using SalesManagement.BL;
 
@@ -24,7 +25,53 @@ namespace SalesManagement.PL {
             cbDiscontinued.Checked = bool.Parse(infomation[6].ToString());
             dateCreateDate.Text = infomation[7].ToString();
         }
-
+        private bool HasExistedProduct(string id) {
+            foreach (var p in Product.GetAllProduct()) {
+                if (p.Id.Equals(id)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+        private bool ValidProduct() {
+            if (txtId.Text.Length == 0) {
+                MessageBox.Show("Product ID cannot empty!");
+                txtId.Focus();
+                return false;
+            }
+            if (!Regex.IsMatch(txtId.Text, @"^P\d{4}$")) {
+                MessageBox.Show("Product ID Invalid! Ex: P0000.");
+                txtId.Focus();
+                return false;
+            }
+            if (HasExistedProduct(txtId.Text)) {
+                MessageBox.Show("Product ID Existed! Try another one.");
+                txtId.Focus();
+                return false;
+            }
+            if (txtName.Text.Length == 0) {
+                MessageBox.Show("Product Name cannot empty!");
+                txtName.Focus();
+                return false;
+            }
+            // Check Category later
+            if (!Regex.IsMatch(txtUnit.Text, @"^unit-\d+$")) {
+                MessageBox.Show("Product ID Invalid! Ex: unit-0.");
+                txtUnit.Focus();
+                return false;
+            }
+            if (!Regex.IsMatch(txtPrice.Text, @"^\d+$") || int.Parse(txtPrice.Text) <= 0) {
+                MessageBox.Show("Price must be an integer and greater zero.");
+                txtPrice.Focus();
+                return false;
+            }
+            if (!Regex.IsMatch(txtQuantity.Text, @"^\d+$") || int.Parse(txtQuantity.Text) <= 0) {
+                MessageBox.Show("Quantity must be an integer and greater zero.");
+                txtQuantity.Focus();
+                return false;
+            }
+            return true;
+        }
         private void btnUpdate_Click(object sender, EventArgs e) {
             string id = txtId.Text;
             string name = txtName.Text;
